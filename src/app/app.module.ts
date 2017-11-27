8364ac5a4bbfdd5b66aa32b098b0fea020dd5665
marcelo <marcelo@virtual-pc>
2017-11-27 10:48:38 -0300

    Agregando la vista cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cliente/cliente.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent implements OnInit {
+
+    
+  constructor() { 
+  
+  }
+
+  ngOnInit() { 
+      
+  }
+
+}
