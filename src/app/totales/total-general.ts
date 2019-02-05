aefb937d43ddd48760b3972b3ba5cbdef2a8fde3
marcelo <marcelo@virtual-pc>
2019-02-05 14:10:57 -0300

    Creando la Vista Total General

---
+++ src/app/totales/total-general.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import {Compra, Impositivo } from '../modelo/Compra';
+
+@Component({
+    selector: 'total-general',
+    templateUrl: './total-general.html'
+})
+
+export class TotalGeneral implements OnInit{
+    
+    @Input() impositivo$: Observable<Impositivo>;
+    impositivo: Impositivo;
+    iva5 = 0 ;
+    iva10= 0 ;
+    constructor() {
+        this.impositivo = {totalExcentas: 0, totalIva5: 0, totalIva10: 0, totalGeneral:0};
+    }
+    
+    ngOnInit(){
+        this.impositivo$.subscribe(data => {
+            this.impositivo = data;
+            this.iva5 = data.totalIva5 / 21;
+            this.iva10 = data.totalIva10 / 11;
+        })
+    }
+}
